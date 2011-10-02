4336953b75be4068da78d88c19d478ef1327265d
Tami Evnin <tami.evnin@gmail.com>
2011-10-02 02:02:46 -0400

    search works aka kenneth is a rockstar
    Signed-off-by: Tami Evnin <tami.evnin@gmail.com>

--- nyt/index.php
+++ nyt/index.php
@@ -11,14 +11,14 @@
 	
 	$testForLatestNYT = $times->findNYTimes($sys_argument);
 	
-	$searchQuery = preg_replace(' ', '+', $_REQUEST('sys_argument'));
+	$searchQuery = preg_replace(' ', '+', $sys_argument);
 
 
 
 	class NYTimes {
 	
 	    public function findNYTimes($query) {
-	        $data = file_get_contents('http://api.nytimes.com/svc/search/v1/article?format=json&query='.urlencode($searchQuery).'&rank=newest&api-key=52697f63c9ade478ec6f2c7d71811aa6:17:61363877');
+	        $data = file_get_contents('http://api.nytimes.com/svc/search/v1/article?format=json&query='.urlencode($query).'&rank=newest&api-key=52697f63c9ade478ec6f2c7d71811aa6:17:61363877');
 	        $nyts = json_decode($data);
 
 	        return $nyts;
