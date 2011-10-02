cef54cf377051c740677f3023b8c1ff4a41bbde6
Nidhi <nidhi.m@gmail.com>
2011-10-01 23:27:53 -0400

    trying to push to arrays

--- nyt/index2.php
+++ nyt/index2.php
@@ -9,8 +9,11 @@
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 		
 		<script>
+ 			var titles = new Array();
+			var text = new Array();
 			var apiKey = "52697f63c9ade478ec6f2c7d71811aa6:17:61363877";
-			var url = "http://api.nytimes.com/svc/search/v1/article?format=json&query=occupy%2Bwall%2Bstreet&rank=newest&api-key=" + apiKey;
+			var url = 
+			"http://api.nytimes.com/svc/search/v1/article?format=json&query=occupy%2Bwall%2Bstreet&rank=newest&api-key="  + apiKey;
 			console.log(url);
 			
 			$.ajax({	
@@ -31,10 +34,14 @@
 				var items = data.value.items[0];
 				for( var i = 0; i<items.results.length; i++)
 				{
+			titles.push(items.results[i].title);
+		 		text.push(items.results[i].body);
 				document.write("<h4>"+items.results[i].title+"</h4>");
+				document.write("<p>"+items.results[i].body+"</p>");
 				}
 			}	
 			});
+			
 			
 		</script>
 		
