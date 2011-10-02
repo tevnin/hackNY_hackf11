ad30f18227db7a34dfc28bacbefaa19c0d9ce262
Tami Evnin <tami.evnin@gmail.com>
2011-10-02 01:06:46 -0400

    things be broken
    Signed-off-by: Tami Evnin <tami.evnin@gmail.com>

---
+++ nyt/index2.php
@@ -0,0 +1,27 @@
+<?php
+
+	$times = new NYTimes();
+	$testForLatestNYT = $times->findNYTimes('occupy+wall+street');
+	// $text = $testForLatestNYT[0]->text;
+	// 	$text = strip_tags(trim($text));
+	// 	echo($text);
+
+	var_dump($testForLatestNYT);
+	
+
+	class NYTimes {
+	
+	    public function findNYT($query) {
+	        $data = file_get_contents('http://api.nytimes.com/svc/search/v1/article?format=json&query='.urlencode($query).'&rank=newest&api-key=52697f63c9ade478ec6f2c7d71811aa6:17:61363877');
+	        $nyts = json_decode($data);
+
+	        return $nyts;
+	    }
+
+	}
+	
+	// $sys_argument = $_REQUEST['sys_argument'];
+	// 	 echo "<message><content>'$sys_argument'!</content></message>";
+	
+	
+?>
